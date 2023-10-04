59984a886d165678988c04ed64ec3ead030b7050
Konstantin <konstantinbagatelia@mail.ru>
2023-10-04 20:18:39 +0700

    cart to db

--- eshop/cart/cart.py
+++ eshop/cart/cart.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+from cart.models import Cart, Order, CartItem, OrderItem
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -18,6 +18,13 @@ class Cart():
         else:
             self.cart[product_id]['quantity'] += quantity
         self.save()
+
+    def get_cartitem_list(self) -> list[CartItem]:
+        cart_items = []
+        for cart_item in self.cart:
+            cart_item = CartItem(cart=self.cart, quantity=self.cart[cart_item]['quantity'])
+            cart_items.append(cart_item)
+        return cart_items
 
     def save(self):
         self.session[settings.CART_SESSION_ID] = self.cart
--- eshop/cart/models.py
+++ eshop/cart/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from items.models import Item
 from personalpage.models import CustomUser
 
-class Cart(models.Model):
+class ModelCart(models.Model):
+    user = models.ForeignKey(CustomUser, verbose_name="Пользователь", on_delete=models.CASCADE)
+
+    def save_to_db(self):
+        pass
+    pass
+
+class ModalOrder(models.Model): 
     user = models.ForeignKey(CustomUser, verbose_name="Пользователь", on_delete=models.CASCADE)
     pass
 
-class Order(models.Model): 
-    user = models.ForeignKey(CustomUser, verbose_name="Пользователь", on_delete=models.CASCADE)
-    pass
+
+
 
 class BaseItem(models.Model):
     item = models.ForeignKey(Item, verbose_name='Товар', on_delete=models.CASCADE)
@@ -18,9 +24,9 @@ class BaseItem(models.Model):
         abstract = True 
 
 class CartItem(BaseItem):
-    cart = models.ForeignKey(Cart, verbose_name="Корзина", on_delete=models.CASCADE)
+    cart = models.ForeignKey(ModelCart, verbose_name="Корзина", on_delete=models.CASCADE)
     pass
 
 class OrderItem(BaseItem):
-    order__save = models.ForeignKey(Order, verbose_name="Заказ", on_delete=models.CASCADE)
+    order__save = models.ForeignKey(ModalOrder, verbose_name="Заказ", on_delete=models.CASCADE)
     pass
